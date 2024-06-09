07ee8c9e3b133101486d5ebbbe6bb44533ec311d
Spectral Platypus <spectralplat@protonmail.com>
2024-06-09 00:50:44 -0400

    Initial Commit

---
+++ Patches.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace TackleboxDbg
+{
+    [HarmonyPatch]
+    internal class Patches
+    {
+        [HarmonyPatch(typeof(Manager), "Update")]
+        [HarmonyPostfix]
+        public static void Update(Manager __instance)
+        {
+            if (Input.GetKeyDown(KeyCode.F11))
+            {
+                __instance.ToggleDebugEnabled();
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using UnityEngine;
+
+namespace TackleboxDbg
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("The Big Catch Tacklebox.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            var harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            try
+            {
+                harmony.PatchAll();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError((object)("Failed to patch: " + ex));
+            }
+        }
+
+    }
+}
