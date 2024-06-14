da84940028aeb4999289754e29917c240124585b
Spectral Platypus <spectralplat@protonmail.com>
2024-06-14 16:09:37 -0400

    Plugin metadata

--- Plugin.cs
+++ Plugin.cs
@@ -4,16 +4,23 @@ using System;
 
 namespace TackleboxDbg
 {
-    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    static class ModInfo
+    {
+        public const string PLUGIN_GUID = "TackleboxDbg";
+        public const string PLUGIN_NAME = "TackleboxDbg";
+        public const string PLUGIN_VERSION = "1.1.0";
+    }
+
+    [BepInPlugin(ModInfo.PLUGIN_GUID, ModInfo.PLUGIN_NAME, ModInfo.PLUGIN_VERSION)]
     [BepInProcess("The Big Catch Tacklebox.exe")]
     public class Plugin : BaseUnityPlugin
     {
         private void Awake()
         {
             // Plugin startup logic
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            Logger.LogInfo($"Plugin {ModInfo.PLUGIN_NAME} is loaded!");
 
-            var harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            var harmony = new Harmony(ModInfo.PLUGIN_GUID);
             try
             {
                 harmony.PatchAll();
