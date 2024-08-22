10d2093c8cbf4197ed319ffd800ddc462293812e
Spectral Platypus <spectralplat@protonmail.com>
2024-08-21 22:03:50 -0400

    Remappable inputs, functions for cleaning up shredders and respawning collectibles

--- Patches.cs
+++ Patches.cs
@@ -1,4 +1,6 @@
 ﻿using HarmonyLib;
+using System.Reflection;
+using System.Reflection.Emit;
 using UnityEngine;
 
 namespace TackleboxDbg
@@ -6,15 +8,116 @@ namespace TackleboxDbg
     [HarmonyPatch]
     internal class Patches
     {
-        [HarmonyPatch(typeof(Manager), "Update")]
-        [HarmonyPostfix]
-        public static void Update(Manager __instance)
+        [HarmonyTranspiler]
+        [HarmonyPatch(typeof(Coin), "CollectRoutine", MethodType.Enumerator)]
+        static IEnumerable<CodeInstruction> TranspileMoveNext(IEnumerable<CodeInstruction> instructions, ILGenerator il)
         {
-            if (Input.GetKeyDown(KeyCode.F11))
+            var codes = new List<CodeInstruction>(instructions);
+            int index = codes.FindLastIndex(c => c.opcode == OpCodes.Call);
+            if (index == -1 || !codes[index].Calls(typeof(Coin).GetMethod(nameof(Coin.Collect))))
             {
-                __instance.ToggleDebugEnabled();
+                return instructions;
             }
 
+            var stateField = codes.Find(c=>c.opcode == OpCodes.Ldfld)?.operand as FieldInfo;
+            if(stateField == null)
+            {
+                return instructions;
+            }
+
+            var corType = stateField.DeclaringType;
+            var setPosType = AccessTools.Method(typeof(Transform), "set_position");
+
+            var startPos = AccessTools.Field(corType, "<StartPos>5__4");
+            codes.Insert(index++, new CodeInstruction(OpCodes.Ldloc_1));
+            codes.Insert(index++, new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(ManagedBehaviour), "get__transform")));
+            codes.Insert(index++, new CodeInstruction(OpCodes.Ldarg_0));
+            codes.Insert(index++, new CodeInstruction(OpCodes.Ldfld, startPos));
+            codes.Insert(index++, new CodeInstruction(OpCodes.Callvirt, setPosType));
+
+            return codes.AsEnumerable();
+        }
+
+        [HarmonyTranspiler]
+        [HarmonyPatch(typeof(DearImGuiDemo), "<OnLayout>g__SaveButton|57_30")]
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator il)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+            int brOpCode = -1;
+            int endFinallyOpCode = -1;
+            Label return0255Label = il.DefineLabel();
+            for (var i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Ldc_I4_0)
+                {
+                    brOpCode = i;
+                }
+                else if (codes[i].opcode == OpCodes.Endfinally)
+                {
+                    endFinallyOpCode = i;
+                    codes[endFinallyOpCode + 1].labels.Add(return0255Label);
+                    break;
+                }
+            }
+            if (brOpCode > -1 && endFinallyOpCode > -1)
+            {
+                codes[brOpCode] = new CodeInstruction(OpCodes.Br, return0255Label);
+            }
+
+            return codes.AsEnumerable();
+        }
+
+        static MethodInfo dynMethod = typeof(PlayerMachine).GetMethod("BestValidGroundFromHistory", BindingFlags.NonPublic | BindingFlags.Instance);
+        [HarmonyPatch(typeof(CheckpointUI), nameof(Checkpoint.ManagedFixedUpdate))]
+        [HarmonyPostfix]
+        public static void ManagedFixedUpdate(CheckpointUI __instance)
+        {
+            if (!Plugin.OverrideDebugArrow.Value)
+            {
+                return;
+            }
+
+            var currentGround = __instance._artParent.gameObject;
+            if (!Manager._instance._debugEnabled)
+            {
+                currentGround.SetActive(false);
+                return;
+            }
+            if (Manager.GetMainMenu().IsMenuActive())
+            {
+                currentGround.SetActive(value: false);
+                return;
+            }
+            if (Manager.GetCutsceneCoordinator()._cutscenePlaying.GetValue())
+            {
+                currentGround.SetActive(value: false);
+                return;
+            }
+            PlayerMachine playerMachine = Manager.GetPlayerMachine();
+            if (!playerMachine)
+            {
+                return;
+            }
+            MainCamera mainCamera = Manager.GetMainCamera();
+            Vector3 difference;
+            bool hit;
+            RaycastHit info;
+            if ((bool)mainCamera)
+            {
+                Vector3 vector = (Vector3)dynMethod.Invoke(playerMachine, new object[] { 0.5f });
+                Vector3 a = vector + -playerMachine._gravityDirection * 4f;
+                difference = a - mainCamera._transform.position;
+                hit = Physics.Raycast(mainCamera._transform.position, difference.normalized, out info, float.PositiveInfinity, Manager.GetPhysicsConfig().Ground, QueryTriggerInteraction.Ignore);
+                if (mainCamera.FacingTowards(vector) && (!hit || (info.distance >= difference.magnitude)))
+                {
+                    currentGround.SetActive(value: true);
+                    __instance._artParent.anchoredPosition = Manager.GetUICamera().WorldSpaceToCanvasPosition(vector);
+                }
+                else
+                {
+                    currentGround.SetActive(value: false);
+                }
+            }
         }
     }
 }
--- Plugin.cs
+++ Plugin.cs
@@ -1,6 +1,8 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
-using System;
+using System.Reflection;
+using UnityEngine;
 
 namespace TackleboxDbg
 {
@@ -8,13 +10,21 @@ namespace TackleboxDbg
     {
         public const string PLUGIN_GUID = "TackleboxDbg";
         public const string PLUGIN_NAME = "TackleboxDbg";
-        public const string PLUGIN_VERSION = "1.1.0";
+        public const string PLUGIN_VERSION = "1.2.0";
     }
 
     [BepInPlugin(ModInfo.PLUGIN_GUID, ModInfo.PLUGIN_NAME, ModInfo.PLUGIN_VERSION)]
     [BepInProcess("The Big Catch Tacklebox.exe")]
     public class Plugin : BaseUnityPlugin
     {
+
+        #region KEYBINDS
+        ConfigEntry<KeyboardShortcut> ToggleDebugKey;
+        ConfigEntry<KeyboardShortcut> RespawnCollectiblesKey;
+        ConfigEntry<KeyboardShortcut> ClearShreddersKey;
+        public static ConfigEntry<bool> OverrideDebugArrow;
+        #endregion
+
         private void Awake()
         {
             // Plugin startup logic
@@ -29,7 +39,72 @@ namespace TackleboxDbg
             {
                 Logger.LogError((object)("Failed to patch: " + ex));
             }
+
+            ToggleDebugKey = Config.Bind("Inputs", "ToggleDebug", new KeyboardShortcut(KeyCode.F11), "The key for toggling Debug HUD");
+            RespawnCollectiblesKey = Config.Bind("Inputs", "RespawnCollectibles", new KeyboardShortcut(KeyCode.F10), "Respawn Coins/Fish");
+            ClearShreddersKey = Config.Bind("Inputs", "ClearShredders", new KeyboardShortcut(KeyCode.F9), "Despawn untethered shredders");
+            OverrideDebugArrow = Config.Bind("Misc", "OverrideDbgArrow", true, "Changes Debug Arrow behavior to display the respawn area");
         }
 
+        private void Update()
+        {
+            if (ToggleDebugKey.Value.IsDown())
+            {
+                Manager._instance.ToggleDebugEnabled();
+            }
+            if (RespawnCollectiblesKey.Value.IsDown())
+            {
+                ResetCollectibles();
+            }
+            if(ClearShreddersKey.Value.IsDown())
+            {
+                BurrowShredders();
+            }
+        }
+
+        static void ResetCollectibles()
+        {
+            var captList = FindObjectsByType<IntroWalkingFish>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+            var currentCapt = Manager.GetPlayerMachine()._currentCapturingCapturable;
+            foreach (var fish in captList)
+            {
+                if (fish._capturable == currentCapt)
+                    continue;
+                if (fish._capturable._state == Capturable.State.Captured)
+                {
+                    fish._agent.Enable();
+                    fish._capturable._state = Capturable.State.NotCapturing;
+                    fish.PlayerRespawned(null);
+                    fish._gameObject.SetActive(true);
+                }
+            }
+            var coinList = FindObjectsByType<Coin>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+            var coinMgr = Manager.GetCoinManager();
+            foreach (var coin in coinList)
+            {
+
+                if (!coin._collectible._collected ||
+                        coinMgr._coinPoolMedium._entries.Contains(coin) || coinMgr._coinPoolMedium._entries.Contains(coin) || coinMgr._coinPoolLarge._entries.Contains(coin))
+                    continue;
+                coin._collectible._collected = false;
+                coin._artObject.SetActive(true);
+                coin._gameObject.SetActive(true);
+            }
+
+            coinMgr._coinPoolSmall.DisableAll();
+            coinMgr._coinPoolMedium.DisableAll();
+            coinMgr._coinPoolLarge.DisableAll();
+        }
+
+        static FieldInfo activeShredders = AccessTools.DeclaredField(typeof(ShredderManager), "_activeShredders");
+        static void BurrowShredders()
+        {
+            var shredMgr = FindFirstObjectByType<ShredderManager>();
+            var activeShredderList = activeShredders.GetValue(shredMgr) as List<SandShredder>;
+            foreach(var shredder in activeShredderList)
+            {
+                shredder.Burrow();
+            }
+        }
     }
 }
